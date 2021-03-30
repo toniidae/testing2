9ff0b78c024ddaa51bded879d28900514f623b07
toniidae <mottjr2202@gmail.com>
2021-03-30 18:53:49 -0300

    tentativa 3 já

--- vamo ve se deu certo.R
+++ vamo ve se deu certo.R
@@ -1,3 +1,6 @@
 print("eu realmente espero que agora de certo")
 print("já fiz isso uma ves mas não deu em nada")
 print('não sei o que eu estou fazendo de errado"')
+
+print('fui um pouco afobado')
+print('vamos tentar de volta')
